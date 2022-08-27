1c741a8b2204c52e620f57bf3fc7ddd579ec3f43
Jet Hughes <jethughes0@gmail.com>
2022-08-27 22:47:22 +1200

    added user schema

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+// create a schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password_hash: { type: String, reqired: false },
+    current: { type: Number, required: false },
+    history: {type: Array, required: false}
+})
+
+userSchema.methods.generateHash = function(password) {
+return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+// compile the schema into a model (named 'message')
+const User = mongoose.model('user', userSchema);
+
+// export the model
+module.exports = User;
