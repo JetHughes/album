9741fd19995948fb151b676e8186747c29287ffa
Jet Hughes <jethughes0@gmail.com>
2022-09-01 23:25:55 +1200

    Added album update script

--- scripts/update_albums.js
+++ scripts/update_albums.js
@@ -22,21 +22,23 @@ const User = require('../models/user');
 User.find({}, async function(err, docs){
 
     docs.forEach(async function(user) {
+        //move the current album to the history
+        user.history.push(user.current)
+
+        //generate another album without duplicates
         let next = Math.floor(Math.random() * 13);
         while (user.history.includes(next)) {
             next = Math.floor(Math.random() * 13);
         }
 
+        //logging
         console.log("\n" + user.username + " " + user._id)
         console.log("next: " + next)
-        
-        user.history.push(user.current)
-        
         user.history.forEach(item => {
             console.log(item)
         })
 
-
+        //update the user
         const db_info = await User.updateOne({_id: user._id}, {$set: {current: next, history: user.history}})
         console.log(db_info)
     });
